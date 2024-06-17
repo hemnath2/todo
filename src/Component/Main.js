8ba8da7486cacbf64db81bc142650907c31b901b
unknown <hemnathsin@gmail.com>
2024-06-17 12:54:13 +0530

    added more features

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Sub from './Sub';
 
 export default function Main(props) {
@@ -23,7 +23,7 @@ export default function Main(props) {
     return (
     <div className='child'>
         <div className='top'>
-        <input type='text' value={val} onChange={(e)=>{setValue(e.target.value)}} /> 
+        <input type='text' value={val} maxLength={30} onChange={(e)=>{setValue(e.target.value)}} /> 
         {/* Getting input from user */}
         <button onClick={adding}>Add</button>
         </div>
@@ -34,18 +34,38 @@ export default function Main(props) {
               
                 
             }
+            
+            let addon=(e)=>{
+              e.preventDefault();
+                    setValue2(e.target.value);
+            }
+
+
             let edit=()=>{
-               let b = prompt('Enter the details ', x);
-               if(b !== ''){
-                let arr3 = arr;
-                arr3[i] = b; 
+               let b = prompt('Enter the value to edit ', x);
+               if(b !== null){
+                let arr3 = [...arr];
+                arr3[i] = b;
                 setArr(arr3);
+                console.log(arr3);
                }
                else{
                  alert("Please enter the value don't leave the value as empty");
                }
             }
-            return(<Sub x={x} del={del} edit={edit} val2={val2}/>)
+            let change1=(e)=>{
+              let data3 = e.target.value;
+              let arr3 = [...arr];
+              arr3[i] = e.target.value;
+              setArr(arr3);
+              console.log(e.target.value); 
+  
+             }
+
+
+            return(<Sub x={x} del={del} edit={edit} val2={val2} addon={addon}
+              inputField = {<input type='text' maxLength={30}  className='chitti' value={x} placeholder={x} onChange={change1}/> }
+            />)
         })}
     </div>
   )
--- src/Component/Sub.js
+++ src/Component/Sub.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Sub(props) {
+
+  let style1 = {textDecoration: 'none'};
+ 
+  let change=(e)=>{
+    //  e.preventDefault();
+    //  setCount(count+1);
+    //  if(count%2==0){
+    //   setData(true);
+    //   console.log(count+''+data);
+    //  }
+    //  else{
+    //   setData(false);
+    //   console.log(count+''+data);
+    //  }
+     setData(!data);
+  }
+
+
+  let [data, setData] = useState(false);
+  let [count, setCount] = useState(0);
+
+  let [data2, setData2] = useState();
+
+  let [E1, setE1] = useState(false);
+
+  let editor=()=>{
+      setE1(true);
+  }
+
+ let submitor=()=>{
+  setE1(false);
+ }
+
+
   return (
     <div className='com'>
       <div className='hello'>
-      <input type='checkbox' onChange={props.del} value={props.val2} />
-      <span>{props.x}</span>
-      </div>
+      <input type='checkbox' onChange={()=>{setData(!data)}} value={data}  />
+      <span style={data == true ? {textDecoration:'line-through', color:'#7A7777'} : {textDecoration:'none'}}>
+        {E1 === false?props.x:  
+          props.inputField
+          
+        }
+        </span>
+      {E1 === false? <button id='edit' onClick={editor}>Edit</button> :
+      <button id='edit' onClick={submitor}>submit</button>}
+      <button id='del' onClick={props.del}>Delete</button>
+      {/* Delete button added for deleting the tasks 16/06  */}
+    </div>
     </div>
   )
 }
